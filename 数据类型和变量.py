a3eff51c99168a1f63b6d16f2788cb5927c49391
hellew <wjt_me@163.com>
2017-08-10 22:27:07 +0800

    tijiao

---
+++ 数据类型和变量.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+print(10 // 3)
+print(10 % 3)
+print(10 / 3)
+
+a = ord('A')
+b = ord('中')
+print(a)
+print(b)
+print(chr(a))
+print(chr(b))
+
+r = (85-72)/72*100
+print('%.1f ' % r)
